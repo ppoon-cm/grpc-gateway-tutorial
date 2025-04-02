9a54ce43993bf1887486b89fd4c05ae218834d69
Patrick Poon <ppoon@clearmotion.com>
2025-04-02 17:46:37 +0000

    Move main.go to cmd directory

---
+++ service/hello/hello.go
@@ -0,0 +1,21 @@
+package hello
+
+import (
+	"context"
+
+	helloworldpb "github.com/myuser/myrepo/gen/go"
+)
+
+type server struct{
+	helloworldpb.UnimplementedGreeterServer
+}
+
+func NewServer() *server {
+	return &server{}
+}
+
+func (s *server) SayHello(ctx context.Context, in *helloworldpb.HelloRequest) (*helloworldpb.HelloReply, error) {
+	return &helloworldpb.HelloReply{Message: in.Name + " world"}, nil
+}
+
+
